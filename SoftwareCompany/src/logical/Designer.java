55102d7e4ac308642afde133aee959e40b8ea578
edgar <edgar@Edga-Garco>
2019-03-18 16:59:22 -0700

    Cambio especialidad to master

--- SoftwareCompany/src/logical/Designer.java
+++ SoftwareCompany/src/logical/Designer.java
@@ -1,18 +1,18 @@
 package logical;
 
 public class Designer extends Worker {
-	private String especialidad;
+	private String master;
 
 	public Designer(String id, String name, String last_name, String address, String gender, int age, float salary, String project, String calification, String especialidad) {
 		super(id, name, last_name, address, gender, age, salary, project, calification);
-		this.especialidad = especialidad;
+		this.master = master;
 	}
 
 	public String getEspecialidad() {
-		return especialidad;
+		return master;
 	}
 
 	public void setEspecialidad(String especialidad) {
-		this.especialidad = especialidad;
+		this.master = master;
 	}
 }

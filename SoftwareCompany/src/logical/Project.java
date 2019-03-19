b2fa108a3047119fdb03440e439732449d0eb73a
edgar <edgar@Edga-Garco>
2019-03-18 17:11:10 -0700

    nuevo atributo

--- SoftwareCompany/src/logical/Project.java
+++ SoftwareCompany/src/logical/Project.java
@@ -6,12 +6,30 @@ public class Project {
 	private String id;
 	private String name;
 	private ArrayList<Worker> workers;
-	
-	public Project(String id, String name) {
+	private String type;
+
+	public Project(String id, String name, ArrayList<Worker> workers, String type) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.workers = new ArrayList<>();
+		this.workers = workers;
+		this.type = type;
+	}
+
+	public ArrayList<Worker> getWorkers() {
+		return workers;
+	}
+
+	public void setWorkers(ArrayList<Worker> workers) {
+		this.workers = workers;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 	public String getId() {

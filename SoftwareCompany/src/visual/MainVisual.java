6521774ec09d7ea6ba631512f774416ff1535d30
Edgar <e.garco@outlook.com>
2019-03-23 16:32:45 -0700

    Creacion ventana principal y menu parte 1

---
+++ SoftwareCompany/src/visual/MainVisual.java
@@ -0,0 +1,66 @@
+package visual;
+
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class MainVisual {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainVisual window = new MainVisual();
+					Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+				    int height = screenSize.height;
+				    int width = screenSize.width;
+				    window.frame.setSize(width, height-43);
+				    window.frame.setLocationRelativeTo(null);
+				    
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainVisual() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Archivo");
+		menuBar.add(mnNewMenu);
+		
+		JMenu mnProyectos = new JMenu("Proyectos");
+		menuBar.add(mnProyectos);
+		
+		JMenuItem mntmNuevoProyecto = new JMenuItem("Nuevo Proyecto");
+		mnProyectos.add(mntmNuevoProyecto);
+	}
+
+}
